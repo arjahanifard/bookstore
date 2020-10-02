72c750ab5d5420ec815fcdcc881a63ced1fa72cd
phoenix <phoenix@185.252.28.106>
2020-10-02 10:16:36 +0330

    add json

--- index.php
+++ index.php
@@ -17,67 +17,20 @@ require_once 'functions.php';
            ?>
         </p>
         <?php
-        if (isset($_GET['title']) && isset($_GET['author'])){
-            ?>
-        <p>
-            the book you are looking for is
-        </p>
-        <ul>
-            <li>
-                <b>
-                    Title
-                </b>
-                : <?php 
-                echo $_GET['title'];?>
-            </li>
-            <li>
-                <b>
-                    Author
-                </b>
-                :<?php
-                echo $_GET['author'];?>
-            </li>
-        </ul>
-        <?php
-        } else {
-        ?>
-        <p>
-            you are not looking for a book?
-        </p>
-        <?php
-        }
-        $books =[
-            [
-                'title' => 'to kill a mochingbird',
-                'author' => 'harper lee',
-                'available' => true,
-                'pages' => 336,
-                'isbn' => 9780061120084
-            ],
-            [
-                'title' => '1984',
-                'author' => 'george orwell',
-                'available'=> true,
-                'pages'=>267,
-                'isbn'=>9780547249643
-            ],
-            [
-                'title'=>'one hundres years of solitude',
-                'author'=>'gabriel garsia marquez',
-                'available'=>true,
-                'page'=>457,
-                'isbn'=>9785267006323
-            ],
-        ];
+    $booksJson = file_get_contents(__DIR__,'books.json');
+    $books = json_decode($booksJson,true);
+    if (isset($_GET['title'])){
+        echo '<p>you are not looking for a book?</p>';
         
-        ?>
+    }
+    ?>
         <ul>
             <?php
                     foreach ($books as $book):?>
             <li>
-                <?php
-                echo printableTitle($book);
-                ?>
+                <a href="?title=<?php echo $book['title'];?>">
+                    <?php echo printableTitle($book);?>
+                </a>
             </li>
             <?php                    endforeach;?>
         </ul>
